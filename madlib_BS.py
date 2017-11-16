c16c3707ceb08129e74ef9516f37ef0f7d3c957a
Brad03 <33519490+Brad03@users.noreply.github.com>
2017-11-16 09:21:19 -0500

    Add files via upload
    This is the real one

--- madlib_BS.py
+++ madlib_BS.py
@@ -1,4 +1,4 @@
-import time
+
 ### MAD LIB ###
 ### Source: http://www.rainbowresource.com/products/001471i1.jpg
 
@@ -57,4 +57,16 @@ adj2 = input()
 print("Write a noun.")
 noun9 = input()
 
-time.sleep(100)
+
+print("A one act play to be performed by two" + pluralnoun1 + " in this room.")
+print("PATIENT: Thiank you so very much for seeming me, Doctor" + person1+ " on such" + adj1 + " notice.")
+print("DENTIST: What is your problem, young" + noun1 +" ?")
+print("PATIENT: I have a pain in my upper" + noun2 + ", which is giving me a severe" + body1 + " ache.")
+print("DENTIST: Let me take a look. Open your" + body2 + " wide.")
+print("Good. Now I'm going to tap your" + pluralnoun2 + " with my" + noun3 + ".")
+print("PATIENT: Shouldn't you give me a" + noun4 + " killer?")
+print("DENTIST: It's not neccessary yet." + exc1 + " I think I see a" + noun5 + " in your upper" + noun6 + ".")
+print("PATIENT: Are you going to pull my" + noun7 + " out?")
+print("DENTIST: No. I'm going to" + verb1 + " your tooth and put in a temporary" + noun8 + ".")
+print("PATIENT: When do I come back for the" + adj2 + " filling?")
+print("DENTIST: A day after I cash your" + noun9 + ".")
